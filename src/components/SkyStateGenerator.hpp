ef50c457d537a47a5b934962ab34835f7b58d28e
charlydelta <git@charlydelta.de>
2019-08-08 23:54:48 +0200

    Documentation: Adjust documentation to reflect recent API changes

--- src/components/SkyStateGenerator.hpp
+++ src/components/SkyStateGenerator.hpp
@@ -14,6 +14,9 @@
 
 namespace REGoth
 {
+  /**
+   * Describes a certain layer in the sky.
+   */
   struct SkyLayer
   {
     bs::String textureName;
@@ -25,7 +28,7 @@ namespace REGoth
   };
 
   /**
-   * One skystate which can be interpolated into
+   * Describes a given sky state.
    */
   struct SkyState
   {
@@ -57,20 +60,19 @@ namespace REGoth
   /**
    * This class will calculate the ambient light color emitted by the sky.
    *
-   * As in the real world, the color of the ambient light changes depending on the
-   * daytime in the original game. To archive this effect, the game uses a list of
-   * so called `SkyState`s, which describe the look of the sky at a single fixed point
-   * in time. During the day/night cycle, the game then interpolates between two of
-   * those `SkyState`s, the one closest *before* the current time, and the closest
-   * next one. The interpolated color is then applied as ambient lighting and fog
-   * color.
+   * As in the real world, the color of the ambient light changes depending on the daytime in the
+   * original game.  To archive this effect, the game uses a list of so called `SkyState`s, which
+   * describe the look of the sky at a single fixed point in time.  During the day/night cycle, the
+   * game then interpolates between two of those `SkyState`s, the one closest *before* the current
+   * time, and the closest next one.  The interpolated color is then applied as ambient lighting and
+   * fog color.
    *
-   * Fog will also be calculated by this class, since it's parameters are embedded
-   * inside the `SkyState`s as well.
+   * Fog will also be calculated by this class, since it's parameters are embedded inside the
+   * `SkyState`s as well.
    *
-   * Since sky rendering is a pretty complex topic in the original game, this class
-   * contains a lot of magic numbers of which we are not quite sure why many of them
-   * were chosen. Probably only because it looked good.
+   * Since sky rendering is a pretty complex topic in the original game, this class contains a lot of
+   * magic numbers of which we are not quite sure why many of them were chosen.  Probably only
+   * because it looked good.
    */
   class SkyStateGenerator
   {
@@ -79,43 +81,43 @@ namespace REGoth
     virtual ~SkyStateGenerator();
 
     /**
-     * Updates the sky state.
+     * Updates the `SkyState`.
      *
      * @param  dayRatio  Value in range [0, 1) where 0 means start of day and (nearly) 1 end of day.
      *                   For example, if the clock reads 0:00, this would need to be 0.  If the clock
      *                   reads 23:59 this would need to be close to 1.
+     * @return The current sky state after update.
      */
     const std::shared_ptr<SkyState> update(float dayRatio);
 
   private:
     /**
-     * @return Color of the coulds layer as poly (layer 1)
+     * @return Color of the coulds layer as poly (layer 1).
      */
     bs::Color getPolyCloudsLayerColor();
 
     /**
      * Finds the two sky states we need to interpolate between for the current time.
-     * For example, if the current time is 0.28, this will find the sky states for
-     * time 0.25 and the one for 0.30.
      *
-     * The current time is read from the master state.
+     * For example, if the current time is 0.28, this will find the sky states for time 0.25 and the
+     * one for 0.30.  The current time is read from `mCurrentSkyState`.
      *
-     * @return Tuple of indices of the presets to interpolate
+     * @return 2-Tuple of indices of the presets to interpolate
      */
     std::tuple<unsigned int, unsigned int> findPresetIndices() const;
 
     /**
-     * Interpolates the two given sky states and stores the result into the master state.
+     * Interpolates the two given sky states and stores the result into `mCurrentSkyState`.
      */
     void interpolatePresets(const SkyState& p1, const SkyState& p2);
 
     /**
-     * Skystates we're interpolating
+     * Collection of `SkyState` presets to interpolate between.
      */
     const std::vector<SkyState> mSkyStatePresets;
 
     /**
-     * Interpolated skystate
+     * `SkyState` reflecting the current status of the sky.
      */
     std::shared_ptr<SkyState> mCurrentSkyState;
   };
